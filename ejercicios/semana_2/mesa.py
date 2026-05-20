ec282b8731ec22893b79431982fd9d0b599ddce0
1725110551-ai <1725110551@utectulancingo.edu.mx>
2026-05-20 01:02:30 +0000

    CREATED .gitignore

--- ejercicios/semana_2/mesa.py
+++ ejercicios/semana_2/mesa.py
@@ -1,15 +1,25 @@
 class Mesa:
 
-    def __init__(self,material,medidas,color,peso):
+    def __init__(self,material,medidas,color,peso,fecha_de_fabricacion,marca,cantidad_de_patas,num_max_de_personas,resistencia):
         self.material=material
         self.medidas=medidas
         self.color=color
         self.peso=peso
+        self.fecha_de_fabricacion=fecha_de_fabricacion
+        self.marca=marca
+        self.cantidad_de_patas=cantidad_de_patas
+        self.num_max_de_personas=num_max_de_personas
+        self.resistencia=resistencia
 
         print(f"Material de fabricación:{self.material}")
         print(f"Medidas de la mesa:{self.medidas}")
         print(f"Color de la mesa:{self.color}")
         print(f"Peso de la mesa:{self.peso}")
+        print(f"Fecha de fabricación:{self.fecha_de_fabricacion}")
+        print(f"Marca de la mesa:{self.marca}")
+        print(f"Cantidad de patas de la mesa:{self.cantidad_de_patas}")
+        print(f"Número máximo de personas que pueden sentarse en la mesa:{self.num_max_de_personas}")
+        print(f"Resistencia de la mesa:{self.resistencia}")
 
     def cargarla(self):
         print("Cargar la mesa")
@@ -22,7 +32,7 @@ class Mesa:
     def pararse(self):
         print("Pararse sobre la mesa")
 
-Mesa_de_salon = Mesa ("Metal,madera","Al:40cm,An:155cm","blanca","14kg")
+Mesa_de_salon = Mesa ("Metal,madera","Al:40cm,An:155cm","blanca","14kg","2023-11-19","IKEA","4","6","100 kg")
 
 Mesa_de_salon.cargarla()
 Mesa_de_salon.sentarse()
--- ejercicios/semana_2/silla.py
+++ ejercicios/semana_2/silla.py
@@ -1,15 +1,27 @@
 class Silla:
 
-    def __init__(self,material,medidas,color_respaldo,peso):
+    def __init__(self,material,medidas,color_respaldo,peso,marca,fecha_de_fabricacion,resistencia,color_asiento,cantidad_de_patas,ajustable):
         self.material=material
         self.medidas=medidas
         self.color_respaldo=color_respaldo
         self.peso=peso
+        self.marca=marca
+        self.fecha_de_fabricacion=fecha_de_fabricacion
+        self.resistencia=resistencia
+        self.color_asiento=color_asiento
+        self.cantidad_de_patas=cantidad_de_patas
+        self.ajustable=ajustable
 
         print(f"Material de fabricación:{self.material}")
         print(f"Medidas de la silla:{self.medidas}")
         print(f"Color del respaldo:{self.color_respaldo}")
         print(f"Peso de la silla:{self.peso}")
+        print(f"Marca de la silla:{self.marca}")
+        print(f"Fecha de fabricación:{self.fecha_de_fabricacion}")
+        print(f"Resistencia de la silla:{self.resistencia}")
+        print(f"Color del asiento:{self.color_asiento}")
+        print(f"Cantidad de patas:{self.cantidad_de_patas}")
+        print(f"Es ajustable?:{self.ajustable}")
 
     def cargarla(self):
         print("Cargar la silla")
@@ -22,7 +34,7 @@ class Silla:
     def pararse(self):
         print("Pararse sobre la silla")
 
-Silla_de_salon = Silla ("Metal,aluminio,madera,tela","Al:40cm,An:35cm","Negro","18kg")
+Silla_de_salon = Silla ("Metal,aluminio,madera,tela","Al:40cm,An:35cm","Negro","18kg","IKEA","2023-11-19","150 kg","Gris","4","NO")
 
 Silla_de_salon.cargarla()
 Silla_de_salon.sentarse()
