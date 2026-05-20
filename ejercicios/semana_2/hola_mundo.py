046a35855360acc61e2b2ace04d02a205d32015c
1725110551-ai <1725110551@utectulancingo.edu.mx>
2026-05-20 13:49:18 +0000

    CREATED .gitignore

--- ejercicios/semana_2/hola_mundo.py
+++ ejercicios/semana_2/hola_mundo.py
@@ -6,9 +6,25 @@ class NombreClase:
     def metodoUno(self):
         print("Metodo uno")
 
-    def metodoUno(self, variable_uno:int, variable_dos:float)->int:
+    def metodoDos(self, variable_uno:int, variable_dos:float)->int:
+        """
+        Este metodo recibe 2 variables enteras, las suma y regresa el
+        resultado  de la suma
+
+        Args:
+
+        variable_uno:int - Primer numero entero
+        variable_dos:int - Segundo numero entero
+        
+        Return:
+        
+        suma : int - Suma de los dos numeros enteros
+        """
         suma = variable_uno + variable_dos
         return suma
+    
+    def metodoTres(self, variable_tres:str)->None:
+        print(f"Numero de caracteres: {len(variable_tres)}")
 
 nombre_objeto = NombreClase()
 nombre_objeto.metodoUno()
